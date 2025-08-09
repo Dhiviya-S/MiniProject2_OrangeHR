3567793de2ef74314e5d7533b675bac0360657a0
Dhiviya <dhiviyainbox01@gmail.com>
2025-08-09 22:25:49 +0530

    Orange HRM Automation

---
+++ MiniProject2/Utils/config.py
@@ -0,0 +1,30 @@
+# Utils folder contains reusable data like url, valid and invalid credentials
+BASE_URL="https://opensource-demo.orangehrmlive.com"
+valid_email="Admin"
+valid_password="admin123"
+invalid_email="dhiviya"
+invalid_password="pass123"
+
+# Employee name can be changed based on webpage.Only available names in the webpage should be used
+Employee_name="Charles"
+Employee_name_invalid="5687"
+
+# New user details. Any details can be changed accordingly.
+User_role="Admin"
+Status="Enabled"
+username="Charles287"
+password="mayflower92"
+invalid_username="Hamleys"
+
+# Leave Details
+Entitled_Days=25
+From_Date="2025-08-18"
+To_Date="2025-08-21"
+Leave_Type="CAN - Personal"
+Leave_Status="Scheduled"
+
+# Claim Details
+Event_Type="Accommodation"
+Currency_Type="Indian Rupee"
+Claim_Date="2025-09-20"
+Claim_Amount=3000
